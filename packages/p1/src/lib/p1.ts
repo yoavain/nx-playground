4f78fcf60cf37339aaa59a2a1cfb8e71a953f6ad
Yoav Vainrich <yoavain@gmail.com>
2022-04-29 17:34:28 +0300

    Trying to work with lock-file

--- packages/p1/src/lib/p1.ts
+++ packages/p1/src/lib/p1.ts
@@ -1,3 +1,9 @@
+import chalk from "chalk";
+
 export function p1(): string {
   return 'p1';
+}
+
+export function logBlue(message: string) {
+  console.log(chalk.blue(message));
 }
--- packages/p2/src/lib/p2.ts
+++ packages/p2/src/lib/p2.ts
@@ -1,9 +1,10 @@
-import { p1 } from "@yoavain/p1";
+import { logBlue, p1 } from "@yoavain/p1";
 
 export function p2(): string {
   return 'p2';
 }
 
 export function p2p1(): string {
+  logBlue("running p2p1");
   return `p2${p1()}`;
 }
